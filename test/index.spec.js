af98dc9267d791463d4267ceed435dbcd70091c5
merimardones <mgmardones@uc.cl>
2021-07-21 17:47:22 -0400

    test html en desarrollo

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,28 @@
-// importamos la funcion que vamos a testear
-import { myFunction } from '../src/lib/index';
+import landingPage from '../src/js/views/landingPage';
+import loginView from '../src/js/views/loginView';
+import registerView from '../src/js/views/registerView';
+import profileView from '../src/js/views/profileView';
+import feedView from '../src/js/views/feedView';
 
-describe('myFunction', () => {
-  it('debería ser una función', () => {
-    expect(typeof myFunction).toBe('function');
+describe('views performed in dynamic html', () => {
+  it('should return html dynamics elements in each template', () => {
+    const landingView = landingPage();
+    expect(landingView instanceof HTMLElement).toBe(true);
+  });
+  it('login debería ser un template html', () => {
+    const loginPage = loginView();
+    expect(loginPage instanceof HTMLElement).toBe(true);
+  });
+  it('selection debería ser un template html', () => {
+    const registerPage = registerView();
+    expect(registerPage instanceof HTMLElement).toBe(true);
+  });
+  it('signup debería ser un template html', () => {
+    const profilePage = profileView();
+    expect(profilePage instanceof HTMLElement).toBe(true);
+  });
+  it('wall debería ser un template html', () => {
+    const feedPage = feedView();
+    expect(feedPage instanceof HTMLElement).toBe(true);
   });
 });
