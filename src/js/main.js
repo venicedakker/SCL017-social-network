f44c1ca7820aae062b9c1ad7965bb47308b8c95e
Valeria Argomedo <valeria.argomedo@gmail.com>
2021-07-28 10:21:40 -0400

    Arreglos del router y SPA

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,19 @@
-import { router } from './router.js';
+import { router } from "./router.js";
+// Manejo de las rutas
+router();
 
-window.addEventListener('hashchange', () => {
-  router(window.location.hash);
-});
+// // Get change when it goes inside the page (posts feed)
+// const auth = firebase.auth();
+// auth.onAuthStateChanged((user) => {
+//   if (user) {
+//     // const userActive = firebaseFunctions.currentUser();
+//     router('#/feed');
+//   } else {
+//     router('');
+//   }
+// });
 
-// Get change when it goes inside the page (posts feed)
-const auth = firebase.auth();
-auth.onAuthStateChanged((user) => {
-  if (user) {
-    // const userActive = firebaseFunctions.currentUser();
-    router('#/feed');
-  } else {
-    router('');
-  }
-});
+// // obtengo los valores de la coleccion usuario
+// // const getUsers = () => firebase.firestore().collection('users').get();
 
-// obtengo los valores de la coleccion usuario
-// const getUsers = () => firebase.firestore().collection('users').get();
+//---------------------------------------------------------------------------------
